faadd7da614c15d1d8bca197d30aa240e2d69893
luanpatrick10 <86279613+luanpatrick10@users.noreply.github.com>
2022-09-05 23:04:40 -0300

    Novo método !

--- Biblioteca/Matematica/Calculos.cs
+++ Biblioteca/Matematica/Calculos.cs
@@ -6,16 +6,24 @@ using System.Threading.Tasks;
 
 namespace Biblioteca.Matematica
 {
-    public class Calculos
-    {
-        public Calculos() { }
-
-        public double CalcularMediaPonderadaDeDuasNotas(double primeiraNota, double segundaNota, double pesodaDaPrimeiraNota, double pesoDaSegundaNota,int quantidadeDeCasasDecimais)
+    public static class Calculos
+    {        
+        public static double CalcularMediaPonderadaDeDuasNotas(double primeiraNota, double segundaNota, double pesodaDaPrimeiraNota, double pesoDaSegundaNota,int quantidadeDeCasasDecimais)
         {
             double somaDosPesosDasNotas = pesodaDaPrimeiraNota + pesoDaSegundaNota;
             double totalDaMultiplicacaoDaPrimeiraNotaComSeuPeso = primeiraNota * pesodaDaPrimeiraNota;
             double totalDaMultiplicacaoDaSegundaNotaComSeuPeso = segundaNota * pesoDaSegundaNota;
             return Math.Round((totalDaMultiplicacaoDaPrimeiraNotaComSeuPeso + totalDaMultiplicacaoDaSegundaNotaComSeuPeso) / somaDosPesosDasNotas, quantidadeDeCasasDecimais);
+        }
+        public static double CalcularMediaPonderadaDeNotas(double[] notas,double[] pesoDasNotas, int quantidadeDeCasasDecimaisParaArredondar)
+        {
+            double resultadoDaMultiplicacaoDasNotasComSeusPesos = 0;
+            double resultadoDaSomaDosPesos  = 0;            
+            for(int indiceDoPesoDaNota = 0; indiceDoPesoDaNota < pesoDasNotas.Length; indiceDoPesoDaNota++)
+                resultadoDaSomaDosPesos += pesoDasNotas[indiceDoPesoDaNota];
+            for (int indiceDaNotaMultiplicadaPeloPeso = 0; indiceDaNotaMultiplicadaPeloPeso < notas.Length; indiceDaNotaMultiplicadaPeloPeso++)            
+                resultadoDaMultiplicacaoDasNotasComSeusPesos += notas[indiceDaNotaMultiplicadaPeloPeso] * pesoDasNotas[indiceDaNotaMultiplicadaPeloPeso];
+            return resultadoDaMultiplicacaoDasNotasComSeusPesos / resultadoDaSomaDosPesos;
         }
     }
 }
--- ExecutarCodigos/Program.cs
+++ ExecutarCodigos/Program.cs
@@ -1,2 +1,17 @@
-﻿
+﻿using Biblioteca.Matematica;
 
+double notaA = Convert.ToDouble(Console.ReadLine());
+double notaB = Convert.ToDouble(Console.ReadLine());
+double notaC = Convert.ToDouble(Console.ReadLine());
+
+double[] pesoDaNota = new double [3];
+pesoDaNota[0] = 2;
+pesoDaNota[1] = 3;
+pesoDaNota[2] = 5;
+
+double[] notas = new double[3];
+notas[0] = notaA;
+notas[1] = notaB;
+notas[2] = notaC;
+
+Console.WriteLine($"MEDIA = {Calculos.CalcularMediaPonderadaDeNotas(notas, pesoDaNota, 1).ToString("N1")}");
